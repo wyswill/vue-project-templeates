2e79fce40cb048f996775e9437a056dacc66e037
wyswill <1151373571@qq.com>
2019-09-26 17:40:43 +0800

    删除

--- src/api/common.js
+++
@@ -1,9 +0,0 @@
-import {commonRequest} from '@/utils/request'
-// 处理图片上传
-export function getPic(){
-    return commonRequest()+"/upload/img"
-}
-// 返回服务器路径
-export function getUrl(){
-    return commonRequest();
-}
--- src/api/orders.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getMallOrders(data) {
-    return request({
-      url: '/article/create',
-      method: 'post',
-      data
-    })
-  }
