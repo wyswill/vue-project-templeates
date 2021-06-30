97314daa7c4073fa528f1f8443a2805f306e488d
wyswill <1151373571@qq.com>
2021-06-30 15:10:57 +0800

    faet: 删除多余

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,8 @@
  */
 import { Module } from '@nestjs/common';
 import { RpcQueryModule } from '@src/rpc-query/rpc-query.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [RpcQueryModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
