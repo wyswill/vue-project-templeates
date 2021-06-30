e709ee2ede0d7d206d63097f2dc7d23c0a9f01ae
wyswill <1151373571@qq.com>
2021-06-30 14:18:47 +0800

    fet: 结构修改

--- src/grpc.options.ts
+++ src/grpc.options.ts
@@ -6,12 +6,12 @@
  */
 import { Transport, ClientOptions } from '@nestjs/microservices';
 import { join } from 'path';
-
+import pkg from '../package.json'
 export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     url: 'localhost:50051',
-    package: 'aha',// 这里的包名和proto 里的包要一致
-    protoPath: join(__dirname, 'rpc/aha.proto'),
+    package: pkg.name,// 这里的包名和proto 里的包要一致
+    protoPath: join(__dirname, `rpc/${pkg.name}.proto`),
   },
 };
--- test/aha.test.ts
+++ test/aha.test.ts
@@ -5,20 +5,20 @@
  * @LastEditTime: 2021-06-10 18:25:12
  */
 
-import path from 'path';
-import { promisify } from 'util';
-import { aha } from '../src/rpc/bundle';
-import RpcQuery from './base';
-const rq = new RpcQuery({
-  host: 'localhost:50051',
-  packge: 'aha',
-});
-const client = rq.getClient<aha.aha_service>();
-client.login = promisify(client.login);
-async function asd() {
-  const res = await client.login({
-    userInfo: { name: 'aaa', age: 12, mony: 1000 },
-  });
-  console.log(res);
-}
-asd();
+// import path from 'path';
+// import { promisify } from 'util';
+// import { aha } from '../src/rpc/bundle';
+// import RpcQuery from './base';
+// const rq = new RpcQuery({
+//   host: 'localhost:50051',
+//   packge: 'aha',
+// });
+// const client = rq.getClient<aha.aha_service>();
+// client.login = promisify(client.login);
+// async function asd() {
+//   const res = await client.login({
+//     userInfo: { name: 'aaa', age: 12, mony: 1000 },
+//   });
+//   console.log(res);
+// }
+// asd();
